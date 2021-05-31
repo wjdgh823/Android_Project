ef86f3e1bdb1f5bae3740e7378ce0fc56678c6be
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-05-31 22:17:38 +0900

    Initial commit

---
+++ aoppart2chapter01/app/src/main/java/com/wjdgh823/aop_part2_chapter01/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wjdgh823.aop_part2_chapter01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
