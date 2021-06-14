b917b81f7e7d39287a28ffe9c2f2c42bb7a0db55
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-06-14 23:45:43 +0900

    계산기 화면 만들기

--- CalculatorApp/app/src/main/java/com/wjdgh823/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/wjdgh823/calculatorapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.wjdgh823.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun buttonClicked(v: View) {
+
+    }
+
+    fun resultButtonClicked(v: View) {
+
+    }
+
+    fun historyButtonClicked(v: View) {
+
+    }
+
+    fun clearButtonClicked(v: View) {
+
+    }
+
 }
