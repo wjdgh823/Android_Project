c5882c152a40cb35c6b4459df5c5ab0cf1b79c35
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-06-16 00:35:58 +0900

    계산기 프로젝트

--- CalculatorApp/app/src/main/java/com/wjdgh823/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/wjdgh823/calculatorapp/MainActivity.kt
@@ -106,6 +106,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun resultButtonClicked(v: View) {
+        val expressionTexts = expressionTextView.text.split(" ")
+
+        if (expressionTextView.text.isEmpty() || expressionTexts.size == 1) {
+            return
+        }
+
+        if (expressionTexts.size != 3 && hasOperator) {
+            Toast.makeText(this, "아직 완성되지 않은 수식입니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()) {
+            Toast.makeText(this, "오류가 발생했습니다..", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val expressionText = expressionTextView.text.toString()
+        val resultText = calculateExpression()
+
+        resultTextView.text = ""
+        expressionTextView.text = resultText
+
+        isOperator = false
+        hasOperator = false
+
 
     }
 
@@ -124,7 +149,7 @@ class MainActivity : AppCompatActivity() {
         return when (op) {
             "+" -> (exp1 + exp2).toString()
             "-" -> (exp1 - exp2).toString()
-            "x" -> (exp1 * exp2).toString()
+            "*" -> (exp1 * exp2).toString()
             "/" -> (exp1 / exp2).toString()
             "%" -> (exp1 % exp2).toString()
             else -> ""
@@ -136,7 +161,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clearButtonClicked(v: View) {
-
+        expressionTextView.text = ""
+        resultTextView.text = ""
+        isOperator = false
+        hasOperator = false
     }
 
     fun String.isNumber(): Boolean {
