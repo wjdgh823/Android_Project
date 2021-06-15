d016b586b9db5b60bd22b1f0b186ae4d07da1ee1
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-06-15 23:45:24 +0900

    계산 로직 구현

--- CalculatorApp/app/src/main/java/com/wjdgh823/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/wjdgh823/calculatorapp/MainActivity.kt
@@ -2,16 +2,104 @@ package com.wjdgh823.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val expressionTextView: TextView by lazy {
+        findViewById<TextView>(R.id.expressionTextView)
+    }
+
+    private val resultTextView: TextView by lazy {
+        findViewById<TextView>(R.id.resultTextView)
+    }
+
+    private var isOperator = false
+    private var hasOperator = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun buttonClicked(v: View) {
+        when (v.id) {
+            R.id.Button0 -> numberButtonClicked("0")
+            R.id.Button1 -> numberButtonClicked("1")
+            R.id.Button2 -> numberButtonClicked("2")
+            R.id.Button3 -> numberButtonClicked("3")
+            R.id.Button4 -> numberButtonClicked("4")
+            R.id.Button5 -> numberButtonClicked("5")
+            R.id.Button6 -> numberButtonClicked("6")
+            R.id.Button7 -> numberButtonClicked("7")
+            R.id.Button8 -> numberButtonClicked("8")
+            R.id.Button9 -> numberButtonClicked("9")
+            R.id.ButtonPlus -> operatorButtonClicked("+")
+            R.id.ButtonMinus -> operatorButtonClicked("-")
+            R.id.ButtonMulti -> operatorButtonClicked("*")
+            R.id.ButtonDivider -> operatorButtonClicked("/")
+            R.id.ButtonModulo -> operatorButtonClicked("%")
+        }
+    }
 
+    private fun numberButtonClicked(number: String) {
+
+        if (isOperator) {
+            expressionTextView.append(" ")
+        }
+
+        isOperator = false
+
+        val expressionText = expressionTextView.text.split(" ")
+        if (expressionText.isNotEmpty() && expressionText.last().length >= 15) {
+           Toast.makeText(this, "15자리 까지만 사용할 수 있습니다.", Toast.LENGTH_LONG).show()
+            return
+        }  else if (expressionText.last().isEmpty() && number == "0") {
+            Toast.makeText(this, "0은 제일 앞에 올 수 없습니다.", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        expressionTextView.append(number) // 입력된 숫자를 더해서 넣어준다.
+        // TODO resultTextView 실시간으로 계산결과를 넣어야하는 기능
+
+    }
+
+    private fun operatorButtonClicked(operator: String) {
+
+        if (expressionTextView.text.isEmpty()) {
+            return
+        }
+
+        when {
+            isOperator -> {
+                val text = expressionTextView.text.toString()
+                expressionTextView.text = text.dropLast(1) + operator// 맨끝 한자리만 지워준다
+            }
+            hasOperator -> {
+                Toast.makeText(this, "연산자는 한번만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+                return
+            }
+            else -> {
+                expressionTextView.append(" $operator")
+            }
+        }
+
+        val ssb = SpannableStringBuilder(expressionTextView.text)
+        ssb.setSpan(
+            ForegroundColorSpan(getColor(R.color.green)),
+            expressionTextView.text.length - 1,
+            expressionTextView.text.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE) // 연산자를 색깔을 칠해주는 것
+
+        expressionTextView.text = ssb
+
+        isOperator = true
+        hasOperator = true
     }
 
     fun resultButtonClicked(v: View) {
