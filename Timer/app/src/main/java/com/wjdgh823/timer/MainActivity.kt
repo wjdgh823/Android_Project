ae188d37cac56770c077fda013174dfb93914e65
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-06-22 23:46:23 +0900

    타이머 앱 화면 구성

--- Timer/app/src/main/java/com/wjdgh823/timer/MainActivity.kt
+++ Timer/app/src/main/java/com/wjdgh823/timer/MainActivity.kt
@@ -2,10 +2,42 @@ package com.wjdgh823.timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private val remainMinuteTextVIew: TextView by lazy {
+        findViewById<TextView>(R.id.remainMinuteTextView)
+    }
+
+    private val seekBar: SeekBar by lazy {
+        findViewById<SeekBar>(R.id.seekBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        seekBar.setOnSeekBarChangeListener( // 게이지 바를 바꾸면
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                   remainMinuteTextVIew.text = "%02d".format(progress) // 게이지바의 숫자를 입력한다.
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+            }
+        )
     }
 }
