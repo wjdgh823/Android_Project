ab36009906334d6dc0f83df7ea9c4eda1ba27066
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-06-24 23:59:26 +0900

    타이머 앱 화면 구성

--- Timer/app/src/main/java/com/wjdgh823/timer/MainActivity.kt
+++ Timer/app/src/main/java/com/wjdgh823/timer/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.wjdgh823.timer
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.SeekBar
 import android.widget.TextView
 import org.w3c.dom.Text
@@ -12,9 +14,15 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.remainMinuteTextView)
     }
 
+    private val remainSecondsTextView: TextView by lazy {
+        findViewById<TextView>(R.id.remainSecondTextView)
+    }
+
     private val seekBar: SeekBar by lazy {
         findViewById<SeekBar>(R.id.seekBar)
     }
+
+    private var currentCountDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,17 +35,49 @@ class MainActivity : AppCompatActivity() {
         seekBar.setOnSeekBarChangeListener( // 게이지 바를 바꾸면
             object : SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                   remainMinuteTextVIew.text = "%02d".format(progress) // 게이지바의 숫자를 입력한다.
+                    if (fromUser) {
+                        updateRemainTime(progress * 60 * 1000L)
+                    }
+
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
+                    currentCountDownTimer?.cancel()
+                    currentCountDownTimer = null
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    seekBar?: return // seekbar가 null이면 함수 끝낸다.
 
+                    currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+                    currentCountDownTimer?.start()
                 }
             }
         )
     }
+
+    private fun createCountDownTimer(initialMillis:Long): CountDownTimer =
+        object:CountDownTimer(initialMillis, 1000L) { // 1초마다 반응하는 것을 실행하는 함수
+            override fun onTick(millisUntilFinished: Long) {
+                updateRemainTime(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                updateRemainTime(0)
+                updateSeekBar(0)
+            }
+        }
+    @SuppressLint("SetTextI18n")
+    private fun updateRemainTime(remainMillis: Long) {
+        val remainSeconds = remainMillis / 1000
+
+        remainMinuteTextVIew.text = "%02d".format(remainSeconds / 60)
+        remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long) {
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
+    }
+
 }
