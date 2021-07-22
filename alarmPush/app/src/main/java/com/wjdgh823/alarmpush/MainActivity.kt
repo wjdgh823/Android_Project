dcf11006ace5eaf46846cf6d341fafe29301e616
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-07-22 23:56:23 +0900

    푸시알람 초기 화면

--- alarmPush/app/src/main/java/com/wjdgh823/alarmpush/MainActivity.kt
+++ alarmPush/app/src/main/java/com/wjdgh823/alarmpush/MainActivity.kt
@@ -3,6 +3,7 @@ package com.wjdgh823.alarmpush
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +19,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFirebase()
+
     }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseToken.text = task.result
+                }
+            }
+    }
+
 }
