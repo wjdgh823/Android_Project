e311e0f755891b4ac65bb2f714d5e22b213a937d
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-07-28 23:55:25 +0900

    메시지 받는 방법

---
+++ alarmPush/app/src/main/java/com/wjdgh823/alarmpush/MyFirebaseMessagingService.kt
@@ -0,0 +1,16 @@
+package com.wjdgh823.alarmpush
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+
+}
