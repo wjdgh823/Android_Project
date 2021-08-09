0397696e8c7dc7d77dc27963e589b2f1170d50a6
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-08-10 00:33:09 +0900

    알림 구현 과정

--- alarmPush/app/src/main/java/com/wjdgh823/alarmpush/MainActivity.kt
+++ alarmPush/app/src/main/java/com/wjdgh823/alarmpush/MainActivity.kt
@@ -1,3 +1,4 @@
+
 package com.wjdgh823.alarmpush
 
 import androidx.appcompat.app.AppCompatActivity
--- alarmPush/app/src/main/java/com/wjdgh823/alarmpush/MyFirebaseMessagingService.kt
+++ alarmPush/app/src/main/java/com/wjdgh823/alarmpush/MyFirebaseMessagingService.kt
@@ -1,5 +1,9 @@
 package com.wjdgh823.alarmpush
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -11,6 +15,26 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+
+        createNotificationChannel()
+
     }
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+
+    }
+
+    companion object {
+        private const val CHANNEL_NAME = "Emoji Party"
+        private const val CHANNEL_DESCRIPTION = "Emoji Party를 위한 채널"
+        private const val CHANNEL_ID = "Channel Id"
+    }
+
 
 }
