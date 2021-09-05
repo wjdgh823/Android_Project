778335344fe872e79465beabb5d2f43c3b557859
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-09-05 23:50:14 +0900

    d

--- alarmPush/app/src/main/java/com/wjdgh823/alarmpush/MyFirebaseMessagingService.kt
+++ alarmPush/app/src/main/java/com/wjdgh823/alarmpush/MyFirebaseMessagingService.kt
@@ -1,9 +1,14 @@
 package com.wjdgh823.alarmpush
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
 import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -13,21 +18,77 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
         super.onNewToken(p0)
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
 
         createNotificationChannel()
 
+        val type = remoteMessage.data["type"]
+            ?.let { NotificationType.valueOf(it) }
+        val title = remoteMessage.data["title"]
+        val message = remoteMessage.data["message"]
+
+        type ?: return
+
+
+
+        NotificationManagerCompat.from(this)
+            .notify(1, createNotification(type, title, message))  // 일반적인 알림 생성
+
     }
+
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT) // 3번째 인자는 중요도이다.
             channel.description = CHANNEL_DESCRIPTION
 
             (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
                 .createNotificationChannel(channel)
         }
 
+    }
+
+    private fun createNotification(type:NotificationType, title: String?, message: String?): Notification {
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("notificationType", "${type.title} 타입")
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+
+        when(type) {
+            NotificationType.NORMAL -> Unit
+            NotificationType.EXPANDABLE -> {
+                notificationBuilder.setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .bigText(
+                            "🥳🎉🎊🎁🎈🤠😍🤩🕺💃👫👭👬🧑‍🤝‍🧑✌🥳🎉🎊🎁🎈🤠😍🤩"
+                        )
+                )
+            }
+            NotificationType.CUSTOM -> {
+                notificationBuilder
+                    .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+                        .setCustomContentView(
+                            RemoteViews(
+                                packageName,
+                                R.layout.view_custom_notification
+                            ).apply {
+                                setTextViewText(R.id.title, title)
+                                setTextViewText(R.id.message, message)
+                            }
+                        )
+
+
+            }
+        }
+        return notificationBuilder.build()
     }
 
     companion object {
---
+++ alarmPush/app/src/main/java/com/wjdgh823/alarmpush/NotificationType.kt
@@ -0,0 +1,8 @@
+package com.wjdgh823.alarmpush
+
+enum class NotificationType(val title: String, val id: Int) {
+    NORMAL("일반 알림", 0),
+    EXPANDABLE("확장형 알림", 1),
+    CUSTOM("커스텀 알림", 3)
+
+}
